032927509030b04c1b6afd7e9e327549ff44e5db
James R. Wynne III <wynnejr@ornl.gov>
2014-02-27 19:17:32 -0500

    Added support in the keyboard handler that will make capital letters work

--- keyboard.c
+++ keyboard.c
@@ -19,9 +19,9 @@ unsigned char kbdus[128] =
   't','y','u','i','o','p','[',']','\n',/* Enter key */
   0,/* 29   - Control */
   'a','s','d','f','g','h','j','k','l',';',/* 39 */
- '\'','`',0,/* Left shift */
+ '\'','`',90,/* Left shift */
  '\\','z','x','c','v','b','n',/* 49 */
-  'm',',','.','/',0,/* Right shift */
+  'm',',','.','/',91,/* Right shift */
   '*',
    0,	/* Alt */
   ' ',	/* Space bar */
@@ -50,6 +50,48 @@ unsigned char kbdus[128] =
     0,	/* All other keys are undefined */
 };
 
+unsigned char upperKbdus[128] =
+{
+  0,27,'!','@','#','$','%','^','&','&',	/* 9 */
+  '(',')','_','+','\b',/* Backspace */
+  '\t',	/* Tab */
+  'Q','W','E','R',/* 19 */
+  'T','Y','U','I','O','P','{','}','\n',/* Enter key */
+  0,/* 29   - Control */
+  'A','S','D','F','G','H','J','K','L',':',/* 39 */
+ '\"','~',90,/* Left shift */
+ '|','Z','X','C','V','B','N',/* 49 */
+  'M','<','>','?',91,/* Right shift */
+  '*',
+   0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};
+
+size_t isShift = 0;
+
 void keyboard_handler()
 {
 	unsigned char scancode;
@@ -58,13 +100,28 @@ void keyboard_handler()
 	scancode = inportb(0x60);
 	//if top bit is set, then key has just been released
 	//useful for shifts and control/alt
-	if (scancode & 0x80)
+	if ((scancode & 0x80) != 0 )
 	{
-
+		if (scancode == 0xAA || scancode == 0xB6)
+		{
+			isShift = 0;
+			return;
+		}
 	}
 	else
 	{
-		terminal_putchar(kbdus[scancode]);
+		if (kbdus[scancode] == 91 || kbdus[scancode] == 90)
+		{
+			isShift = 1;
+			return;
+		}
+		if (isShift != 0)
+		{
+			terminal_putchar(upperKbdus[scancode]);
+		}
+		else
+			terminal_putchar(kbdus[scancode]);
+
 		if (kbdus[scancode] == '\n')
 		{
 			runShell();
